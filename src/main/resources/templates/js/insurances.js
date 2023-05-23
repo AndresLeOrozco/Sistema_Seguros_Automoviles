34fd64a779456a9af2fb54994074f05a8168d8e3
eduardoojeda <peleojeda@gmail.com>
2023-05-22 21:59:33 -0600

    Register is completed

--- src/main/resources/templates/js/insurances.js
+++ src/main/resources/templates/js/insurances.js
@@ -1,3 +1,13 @@
 class Insurances{
+    dom;
+    modal;
+
+    state;
+
+    constructor() {
+        this.state = {'entities': new Array(), 'entity': "", 'mode': 'A'};
+        this.dom = this.render();
+        this.modal = new bootstrap.Modal(this.dom.querySelector('#modal'));
+    }
 
 }
