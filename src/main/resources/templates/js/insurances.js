d990e1a81ff7543cf3d5371f1871c9c93fb960cd
eduardoojeda <peleojeda@gmail.com>
2023-05-24 01:40:13 -0600

    Muestra todos los seguros agregados y tambien el boton en detalles funciona

--- src/main/resources/templates/js/insurances.js
+++ src/main/resources/templates/js/insurances.js
@@ -8,6 +8,15 @@ class Insurances{
         this.state = {'entities': new Array(), 'entity': "", 'mode': 'A'};
         this.dom = this.render();
         this.modal = new bootstrap.Modal(this.dom.querySelector('#myModal'));
+        this.modalAdd = new bootstrap.Modal(this.dom.querySelector('#myModal1'));
+        this.modalAdd2 = new bootstrap.Modal(this.dom.querySelector('#myModal2'));
+        this.modalAdd3 = new bootstrap.Modal(this.dom.querySelector('#myModal3'));
+        this.modalAdd4 = new bootstrap.Modal(this.dom.querySelector('#myModal4'));
+        this.dom.querySelector('#addNew').addEventListener('click', this.registerInsurance);
+        this.dom.querySelector('#nextV1').addEventListener('click', this.registerInsurance2);
+        this.dom.querySelector('#nextV2').addEventListener('click', this.registerInsurance3);
+        this.dom.querySelector('#nextV3').addEventListener('click', this.registerInsurance4);
+
 
     }
 
@@ -15,6 +24,7 @@ class Insurances{
         const html = `
             ${this.renderList()}
             ${this.renderModal()} 
+            ${this.renderModalAdd()}
         `;
         let rootContent = document.createElement('div');
         rootContent.id = 'insurance';
@@ -28,8 +38,10 @@ class Insurances{
         return `
         <div id="list" class="container">     
             <div class="card bg-light">
-                <h4 class="card-title mt-3 text-center"><span><i class="fas fa-shield-alt"></i></span> Insurances</h4><br>    
-
+                <h4 class="card-title mt-3 text-center"><span><i class="fas fa-shield-alt"></i></span> Insurances</h4><br>
+                    <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+                          <button type="button" class="btn btn-outline-primary" id="addNew">Add New</button>
+                    </div>    
                     <div class="table-responsive " style="max-height: 300px; overflow: auto">
                         <table class="table table-striped table-hover">
                             <thead>
@@ -61,7 +73,7 @@ class Insurances{
       <!-- Modal Header -->
       <div class="modal-header">
         <h4 class="modal-title">Insurance Information</h4>
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       
       <div class="modal-body" id="vehiclePrice">
@@ -87,13 +99,193 @@ class Insurances{
 
       <!-- Modal footer -->
       <div class="modal-footer">
-        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-danger" data-bs-dismiss="modal" aria-label="Close">Close</button>
       </div>
 
     </div>
   </div>
 </div>
 <!--        </div>      -->
+        `;
+    }
+
+    renderModalAdd = () =>{
+        return `
+        <div class="modal fade" id="myModal1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+   <div class="modal-dialog">
+      <div class="modal-content">
+         <div class="modal-header">
+            <h4 class="modal-title" id="myModalLabel">Insurance</h4>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+         </div>
+         <div class="modal-body">
+            <form role="form" action="" method="post" class="registration-form">
+               <fieldset style="display: block;">
+                  <div class="form-top">
+                     <div class="form-top-left">
+                        <h3>Step 1 / 3</h3>
+                        <p>Fill the next information:</p>
+                     </div>
+                     <div class="form-top-right">
+                        <i class="fa fa-truck"></i>
+                        <h7>Vehicle Information</h7>
+                     </div>
+                  </div>
+                  <div class="form-bottom">
+                     <div class="form-group">
+                        <label class="sr-only" for="form-first-name"></label>
+                        <input name="form-first-name" placeholder="Vehicle VIN..." class="form-first-name form-control input-error" id="iVIN" type="text">
+                     </div>
+                     <div class="form-group">
+                        <label class="sr-only" for="form-last-name"></label>
+                        <input name="form-last-name" placeholder="Vehicle Price..." class="form-last-name form-control input-error" id="iPrice" type="text">
+                     </div>
+                     <br>
+                     <i class="fa fa-car"></i>
+                     <h7>Brand - Model - Year</h7>
+                     <select class="form-select" aria-label="Default select example" required>
+                      <option selected>Select Vehicle</option>
+                      <option value="1">One</option>
+                      <option value="2">Two</option>
+                      <option value="3">Three</option>
+                    </select>
+                    <br>
+                    <i class="fa fa-credit-card"></i>
+                    <h7>Pay Method</h7>
+                    <br>
+                    <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
+                      <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off" checked>
+                      <label class="btn btn-outline-primary" for="btnradio1">Radio 1</label>
+                    
+                      <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off">
+                      <label class="btn btn-outline-primary" for="btnradio2">Radio 2</label>
+                    
+                      <input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off">
+                      <label class="btn btn-outline-primary" for="btnradio3">Radio 3</label>
+                    </div>
+            </form>
+            <br><br><br>
+         </div>
+         <div class="modal-footer">
+            <button type="button" class="btn btn-default btn-prev">Prev</button>
+            <button id="nextV1" type="button" class="btn btn-default btn-next">Next</button>
+            <button type="button" class="btn btn-default" data-bs-dismiss="modal" aria-label="Close">Close</button>
+         </div>
+      </div>
+   </div>
+</div>
+</div>
+<!-- Modal -->
+<div class="modal fade" id="myModal2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+   <div class="modal-dialog">
+      <div class="modal-content">
+         <div class="modal-header">
+            <h4 class="modal-title" id="myModalLabel">Modal title 2</h4>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+         </div>
+         <div class="modal-body">
+           <form>
+            <fieldset>
+                  <div class="form-top">
+                     <div class="form-top-left">
+                        <h3>Step 2 / 3</h3>
+                        <p>Set up your account:</p>
+                     </div>
+                     <div class="form-top-right">
+                        <i class="fa fa-key"></i>
+                     </div>
+                  </div>
+                  <div class="form-bottom">
+                     <div class="form-group">
+                        <label class="sr-only" for="form-email">Email</label>
+                        <input name="form-email" placeholder="Email..." class="form-email form-control" id="form-email" type="text">
+                     </div>
+                     <div class="form-group">
+                        <label class="sr-only" for="form-password">Password</label>
+                        <input name="form-password" placeholder="Password..." class="form-password form-control" id="form-password" type="password">
+                     </div>
+                     <div class="form-group">
+                        <label class="sr-only" for="form-repeat-password">Repeat password</label>
+                        <input name="form-repeat-password" placeholder="Repeat password..." class="form-repeat-password form-control" id="form-repeat-password" type="password">
+                  
+               </fieldset>
+           </form>
+         </div>
+         <div class="modal-footer">
+            <button type="button" class="btn btn-default btn-prev">Prev</button>
+            <button id="nextV2" type="button" class="btn btn-default btn-next">Next</button>
+            <button type="button" class="btn btn-default" data-bs-dismiss="modal" aria-label="Close">Close</button>
+         </div>
+      </div>
+   </div>
+</div>
+<!-- Modal -->
+<div class="modal fade" id="myModal3" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+   <div class="modal-dialog">
+      <div class="modal-content">
+         <div class="modal-header">
+            <h4 class="modal-title" id="myModalLabel">Modal title 3</h4>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+         </div>
+         <div class="modal-body">
+           <form>
+            <fieldset>
+                  <div class="form-top">
+                     <div class="form-top-left">
+                        <h3>Step 3 / 3</h3>
+                        <p>Social media profiles:</p>
+                     </div>
+                     <div class="form-top-right">
+                        <i class="fa fa-twitter"></i>
+                     </div>
+                  </div>
+                  <div class="form-bottom">
+                     <div class="form-group">
+                        <label class="sr-only" for="form-facebook">Facebook</label>
+                        <input name="form-facebook" placeholder="Facebook..." class="form-facebook form-control" id="form-facebook" type="text">
+                     </div>
+                     <div class="form-group">
+                        <label class="sr-only" for="form-twitter">Twitter</label>
+                        <input name="form-twitter" placeholder="Twitter..." class="form-twitter form-control" id="form-twitter" type="text">
+                     </div>
+                     <div class="form-group">
+                        <label class="sr-only" for="form-google-plus">Google plus</label>
+                        <input name="form-google-plus" placeholder="Google plus..." class="form-google-plus form-control" id="form-google-plus" type="text">
+                     </div>
+                     <button type="button" class="btn btn-previous">Previous</button>
+                     <button type="submit" class="btn">Sign me up!</button>
+                  </div>
+               </fieldset>
+           </form>
+         </div>
+         <div class="modal-footer">
+            <button type="button" class="btn btn-default btn-prev">Prev</button>
+            <button id="nextV3" type="button" class="btn btn-default btn-next">Next</button>
+            <button type="button" class="btn btn-default" data-bs-dismiss="modal" aria-label="Close">Close</button>
+         </div>
+      </div>
+   </div>
+</div>
+<!-- Modal -->
+<div class="modal fade" id="myModal4" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+   <div class="modal-dialog">
+      <div class="modal-content">
+         <div class="modal-header">
+            <h4 class="modal-title" id="myModalLabel">Modal title 4</h4>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+         </div>
+         <div class="modal-body">
+            Thank For Register
+         </div>
+         <div class="modal-footer">
+            <button type="button" class="btn btn-default btn-prev">Prev</button>
+            <button type="button" class="btn btn-default btn-next">Next</button>
+            <button type="button" class="btn btn-default" data-bs-dismiss="modal" aria-label="Close">Close</button>
+         </div>
+      </div>
+   </div>
+</div>
+        
         `;
     }
     showModal= async ()=>{
@@ -168,5 +360,26 @@ class Insurances{
 
         return price;
     }
+
+    registerInsurance = () => {
+        this.modalAdd.show();
+    }
+    registerInsurance2 =async () => {
+        this.modalAdd2.show();
+        this.modalAdd.hide();
+
+
+    }
+
+    registerInsurance3 =async () => {
+        this.modalAdd3.show();
+        this.modalAdd2.hide();
+    }
+    registerInsurance4 =async () => {
+        this.modalAdd4.show();
+        this.modalAdd3.hide();
+    }
+
+
 
 }
